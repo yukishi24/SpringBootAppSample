7467f746db1adbb307cfd2c83e380eebb21e77cc
yukishi24 <yukishi24github@gmail.com>
2023-01-22 20:17:38 +0900

    add:login file

---
+++ SpringBootAppSample/src/main/java/com/example/controller/SignupController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.application.service.UserApplicationService;
+
+@Controller
+@RequestMapping("/user")
+public class SignupController {
+  @Autowired
+  private UserApplicationService userApplicationService;
+  
+  //ユーザー登録画面の表示
+  @GetMapping("/signup")
+  public String getSignup(Model model) {
+    //性別を取得
+    Map<String ,Integer>genderMap = userApplicationService.getGenderMap();
+    model.addAttribute("genderMap",genderMap);
+    
+    return "user/signup";
+  }
+  @PostMapping("/signup")
+  public String postSignup() {
+    //ログイン画面にリダイレクト
+    return "redirect:/login";
+  }
+}
